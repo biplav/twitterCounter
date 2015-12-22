ff17e4968ad9e5ec8fe956d18528d3c7d662aa9c
Biplav <biplav.saraf@gmail.com>
2015-12-22 16:01:35 +0530

    Update jqueryTwitterCharCounter.js

--- jqueryTwitterCharCounter.js
+++ jqueryTwitterCharCounter.js
@@ -1,21 +1,6 @@
-/* jQuery jqEasyCharCounter plugin
- * Examples and documentation at: http://www.jqeasy.com/
- * Version: 1.0 (05/07/2010)
- * No license. Use it however you want. Just keep this notice included.
- * Requires: jQuery v1.3+
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
 (function($) {
     $.fn.extend({
-        jqEasyCounter: function(givenOptions) {
+        jqEasyTwitterCounter: function(givenOptions) {
             return this.each(function() {
                 var $this = $(this),
                     options = $.extend({
